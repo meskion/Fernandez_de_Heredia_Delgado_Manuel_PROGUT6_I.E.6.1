030511f270627adb182217f164540f328fb22d61
meskion <manufdh@hotmail.com>
2021-01-18 20:20:20 +0100

    Sopa de letras arreglada

--- src/arraysMatrices/Actividad36.java
+++ src/arraysMatrices/Actividad36.java
@@ -9,8 +9,11 @@ import util.Matrix;
 
 public class Actividad36 {
 
+	// Lista de posiciones ocupadas por letras de palabras
 	static DynIntMatrix ocupadas = new DynIntMatrix();
+	// Matriz con caracteres de la sopa
 	static char[][] sopa = new char[15][15];
+	// lista de palabras a colocar en la sopa.
 	static String[] palabras = new String[10];
 
 	public static void main(String[] args) {
@@ -26,8 +29,10 @@ public class Actividad36 {
 
 		}
 
-		Matrix.randFill(sopa, 'A', 'Z');
-
+		Matrix.randFill(sopa, 'A', 'Z' + 1);
+		// String[] palabras = {"hola", "hola", "hola","hola", "hola", "hola","hola",
+		// "hola", "hola","hola", "hola", "hola","hola", "hola","hola", "hola",
+		// "hola","hola","hola", "hola","hola", "hola", "hola","hola"};
 		for (String s : palabras) {
 
 			/*
@@ -42,9 +47,15 @@ public class Actividad36 {
 		}
 
 		System.out.println(Matrix.deepToString(sopa));
-		System.out.println(Matrix.deepToString(ocupadas.get()));
+//		System.out.println(Matrix.deepToString(ocupadas.get()));
 		scan.close();
 	}
+
+	/**
+	 * Metodo que coloca las letras de la palabra s en el tablero aleatoriamente.
+	 * 
+	 * @param s palabra a colocar
+	 */
 
 	static void placeChars(String s) {
 		int sLen = s.length();
@@ -61,10 +72,12 @@ public class Actividad36 {
 			 * comprueba que la palabra colocada no pisa con palabras anteriores, si pisa,
 			 * pero se da el caso que la letra de las dos palabras en esa posicion es la
 			 * misma, la da por valida. En otro caso, vuelve a intentar colocar la palabra
-			 * de 0.
+			 * de 0.i = [0,1] [0,0] [0,1]
 			 */
 			for (int[] i : aux) {
+
 				for (int j = 0; j < sLen; j++) {
+
 					if (sopa[i[0]][i[1]] != s.charAt(j) && Arrays.equals(i, sPos[j])) {
 						wrongPlacedChar = true;
 					}
@@ -83,6 +96,13 @@ public class Actividad36 {
 
 	}
 
+	/**
+	 * Metodo que selecciona sLen posiciones aleatorias seguidas en una direccion
+	 * aleatoria, entre derecha, abajo, diagonal arriba, diagonal abajo.
+	 * 
+	 * @param sLen
+	 * @param sPos
+	 */
 	static void selectCharPos(int sLen, int[][] sPos) {
 		Random rand = new Random();
 		int direction = rand.nextInt(4);
@@ -90,7 +110,7 @@ public class Actividad36 {
 		switch (direction) {
 		case 0:// HORIZONTAL
 			fila = rand.nextInt(15);
-			
+
 			col = rand.nextInt(15 - sLen);
 			// Tabla con posiciones de s
 
@@ -103,7 +123,7 @@ public class Actividad36 {
 
 		case 1:// VERTICAL
 			fila = rand.nextInt(15 - sLen);
-		
+
 			col = rand.nextInt(15);
 			// Tabla con posiciones de s
 
@@ -113,14 +133,15 @@ public class Actividad36 {
 			}
 			break;
 		case 2:// DIAG UP
-			fila = rand.nextInt(15 - sLen);
+			fila = rand.nextInt(15 - sLen) + sLen;
 			// y una columna:
-			col = rand.nextInt(15 - sLen) + sLen;
+			col = rand.nextInt(15 - sLen);
 			// Tabla con posiciones de s
 
 			for (int i = 0; i < sLen; i++) {
-				sPos[i][0] = fila + i;
-				sPos[i][1] = col - i;
+				sPos[i][0] = fila - i;
+				sPos[i][1] = col + i;
+
 			}
 			break;
 		case 3:// DIAG DOWN
